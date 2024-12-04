1b99bd4f6a262450a416e7fb292e859efafbbde2
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-12-04 15:42:33 -0500

    remove pnpm-lock.yaml file

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -8,6 +8,7 @@ import {
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
