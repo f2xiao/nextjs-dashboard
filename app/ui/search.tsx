f27c362d9800702d8c4a0ec25c916b5a4ee46ebe
“snowball” <25191617+f2xiao@users.noreply.github.com>
2024-12-10 15:54:04 -0500

    add debouncing to the search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
     // reads the initial search params from current URL address
     // utility methods for manipulating the URL query parameters.
@@ -24,7 +25,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     // console.log("query", searchParams.get("query"));
     // console.log(term);
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
